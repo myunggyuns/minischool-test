db08e8ad76556dc5ede650b8e75bdcf78e306a53
Son Myunggyun <sonmg90@gmail.com>
2021-03-08 19:43:20 +0900

    onuii-test commit

--- src/redux/reducer/index.tsx
+++ src/redux/reducer/index.tsx
@@ -1,13 +1,6 @@
 import { combineReducers } from "redux";
-import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import { searchReducer } from "./SearchReducer";
-
-const persistConfig = {
-  key: "root",
-  storage,
-};
 
 const rootReducer = combineReducers({ searchReducer });
 
-export default persistReducer(persistConfig, rootReducer);
+export default rootReducer;
