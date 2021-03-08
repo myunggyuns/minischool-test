266cc7742b4f9b3da005ed22f924c45cef7761b3
Son Myunggyun <sonmg90@gmail.com>
2021-03-08 19:48:06 +0900

    onuii commits

--- src/rest/SearchAPI.tsx
+++ src/rest/SearchAPI.tsx
@@ -4,10 +4,11 @@ const BASEURL = "https://api.github.com/search";
 const HEADERS = { Accept: "application/vnd.github.v3+json" };
 
 export const searchRepo = async (entity: string, queryObj: any) => {
-  let url: string = `${BASEURL}/${entity}?${convertQueryParams(queryObj)}`;
+  const url: string = `${BASEURL}/${entity}?${convertQueryParams(queryObj)}`;
+
   try {
-    let response = await fetch(url, { method: "GET", headers: HEADERS });
-    let result = await response.json();
+    const response = await fetch(url, { method: "GET", headers: HEADERS });
+    const result = await response.json();
     if (result.message) {
       alert(`${result.message}  check serach text`);
     }
